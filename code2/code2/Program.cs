cc4322675fc920aadf423aa7a1d6159562d6d2dc
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-05-22 12:23:35 -0700

    comit

---
+++ code2/code2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks = 80;
+            Console.WriteLine(marks);
+            float new_marks = 1;
+            Console.WriteLine(new_marks);
+            long balance = 26383372828838;
+            Console.WriteLine(balance);
+            char name = 'a';
+            Console.WriteLine(name);
+            string name1 = "shahbbaz";
+            Console.WriteLine(name1);
+            bool ismale = true;
+            Console.WriteLine(ismale);
+            Console.ReadKey();
+        }
+    }
+}
