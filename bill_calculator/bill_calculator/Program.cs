92457a4aa7bed6bef626499ff94e8516499ae2a6
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-05-28 07:02:56 -0700

    bill calculator

---
+++ bill_calculator/bill_calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bill_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int telephone = int.Parse(Console.ReadLine());
+            double charji = 2424.23;
+            double smarttv = 2536.22;
+            double discount = 0;
+            string name = "shahbaz hussain";
+            string address = "rawalpindi";
+            double total_bill = telephone+charji+smarttv+discount;
+            Console.WriteLine("your total bill is :" + total_bill);
+            double service_charges = total_bill * 9.9 / 100;
+            Console.WriteLine("your service charges are: " + service_charges);
+            double WTH = service_charges + total_bill * 8.5 / 100;
+            Console.WriteLine("your WTH IS: " + WTH);
+            double total_amount = WTH + service_charges + total_bill;
+            Console.WriteLine("your total bill is " + total_amount +"  for  "+ name+" "+address);
+            Console.ReadKey();
+        }
+
+    }
+}
