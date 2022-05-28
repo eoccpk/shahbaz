c53d5e24bce9b59f234a6dd6c9bd73e219aba911
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-05-28 09:18:00 -0700

    AREA 28-05-2022

---
+++ calculations-1/calculations-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculations_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            Console.WriteLine("INPUT THE RADIUS");
+            double radius = int.Parse(Console.ReadLine());
+            double area;
+            area=pi*(radius*radius);
+            Console.WriteLine("the area is: " + area);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ calculations/calculations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("          SHAHBAZ EOCC");
+            Console.Write("input the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("input the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum= num1+ num2;
+            Console.WriteLine("THE SUM IS :" + sum);
+            int sub = num1 - num2;
+            Console.WriteLine("THE SUBSTRACTION IS :"+sub);
+            int mul = num1 * num2;
+            Console.WriteLine("THE product is : "+ mul);
+            double div;
+            if(num1==0)
+            {
+                Console.WriteLine("result is infinity");
+            }
+            else
+            {
+               div = num1 / num2;
+               Console.WriteLine("THE RESULT IS :" + div);
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
