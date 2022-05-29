5be672b797fea7923c35c3fae70872294e1c1070
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-05-29 06:36:11 -0700

    property_inheritance

---
+++ property-inheritance-/property-inheritance-/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property_inheritance_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inheritance property software ");
+            Console.WriteLine("enter the number of prople");
+            int total_people =int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the property in marlas");
+            double marlas = Convert.ToDouble(Console.ReadLine());
+            double per_peice = marlas / total_people;
+            Console.WriteLine("the per person share for male is: "+per_peice+" marlas");
+            double female_pershare = per_peice / 2;
+            Console.WriteLine("the share for females is         :" +female_pershare+" marlas");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ temperature_converter/temperature_converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperature_converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kindly input the temperature in the celcius");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            double farenheit;
+            farenheit = (celcius * 9 / 5) + 32;
+            Console.WriteLine("the temperature in farenheit is " + farenheit);
+            string str= farenheit.ToString();
+            Console.WriteLine("the temperature is " + (str+"f" +" & "+ celcius+"C"));
+            Console.ReadKey();
+        }
+    }
+}
