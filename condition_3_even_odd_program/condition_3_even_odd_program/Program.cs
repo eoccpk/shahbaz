feb7c2be3426ae1aebb1c249c8c1162f91249a40
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-04 08:39:36 -0700

    check even or odd

---
+++ condition_3_even_odd_program/condition_3_even_odd_program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condition_3_even_odd_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number= int.Parse(Console.ReadLine());
+            if (number%2 !=0)
+            {
+                Console.WriteLine("the number is odd");
+            }
+            else
+                Console.WriteLine("the number is even");
+            Console.ReadKey();
+        }
+    }
+}
