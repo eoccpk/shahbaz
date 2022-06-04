f1efbfd5a1d7d20b8dbcd8d9de465a6fec7999e9
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-04 08:40:48 -0700

    comit 2

---
+++ conditions_2/conditions_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditions_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+            if (num >= 0)
+            {
+                Console.WriteLine("THE NUMBER IS positive ");
+            }
+            else
+                Console.WriteLine("the number is negative");
+            Console.ReadKey();
+        }
+        
+    }
+}
