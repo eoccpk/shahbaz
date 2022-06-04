c1f99f8f89bd91117c93cd3fae72111073f9f6f0
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-04 08:39:59 -0700

    find eligble

---
+++ CONDITION_4_to_find_max_of_3_num/CONDITION_4_to_find_max_of_3_num/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONDITION_4_to_find_max_of_3_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the num1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the num2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the num3");
+            int num3 = int.Parse(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("num1 is greater");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("num2 is greater");
+            }
+            else
+                Console.WriteLine("num3 is the greater");
+            Console.ReadKey();
+        }
+    }
+}
