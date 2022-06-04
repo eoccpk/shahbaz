1b0bd9970b53f124258dc08a4f7187e46e4fc103
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-04 08:41:58 -0700

    comit 4

---
+++ find_biggest_no/find_biggest_no/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_biggest_no
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num=0;
+            double middle=0;
+            double small=0;
+            Console.WriteLine("input the first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("input the second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("input the third number");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            if(num1 > num2 && num1>num3 )
+            {
+                num = num1;
+            }
+            if (num1 < num2 && num1>num3)
+            {
+                num = num2;
+            }
+            if (num1 < num2 && num1<num3)
+            {
+                num = num3;
+            }
+            //checking values of num2
+          if(num2>num1 && num2>num3)
+            {
+                middle = num1;
+            }
+          if(num2<num1 && num2 >num3)
+            {
+                middle = num2;
+            }
+          if(num2<num1 && num2<num3)
+            {
+                middle=num3;
+            }
+            //checking values of num3
+            if (num3 > num1 && num3 > num2)
+            {
+                small = num1;
+            }
+            if (num3 < num1 && num3 > num2)
+            {
+                small = num2;
+            }
+            if (num3 < num1 && num3 < num2)
+            {
+                small = num3;
+            }
+           
+            Console.WriteLine(num1);
+            Console.WriteLine(middle);
+
+            Console.WriteLine(small);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ screenshot/screenshot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace screenshot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("+------------------+");
+            Console.WriteLine("|id|f_name|l_name  |");
+            Console.WriteLine("+------------------+");
+            Console.WriteLine("|1 |shahbaz|hussain|");
+            Console.WriteLine("+------------------+");
+            Console.WriteLine("1 row in set (0.00 sec");
+            Console.ReadKey();
+
+        }
+    }
+}
