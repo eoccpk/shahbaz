234b7d32a90cad77a92a028e98e3de3a50905864
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:59:26 -0700

    ASG_02 Loops

---
+++ ASG_02 LOOPS/ASG_02 LOOPS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_02_LOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for(i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("the counting from 1 to 10 is :" + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
