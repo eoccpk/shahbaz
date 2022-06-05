e73f16a1eb24398ff0b7d00724459e356ce6e348
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 04:07:01 -0700

    ASG_04_LOOPS display sqaure using for loop

---
+++ ASG_04_SQUARES OF THE NUMBERS/ASG_04_SQUARES OF THE NUMBERS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_04_SQUARES_OF_THE_NUMBERS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            double square;
+            for(i= 0; i < 5; i++)
+            {
+                square=i*i;
+                Console.WriteLine("THE NUMBER "+i+" and its square is "+square);
+            }
+            Console.ReadKey();
+        }
+    }
+}
