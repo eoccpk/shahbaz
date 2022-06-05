f3e7a777330a32865ca62ec17193b97310eeb25f
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 04:03:14 -0700

    ASG 03_LOOP_DISPLAY SUM

---
+++ ASG_03/ASG_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            int sum =0;
+            while(i < 5)
+            {
+                sum = sum + i;
+                Console.WriteLine("the number is"+i +"and sum is "+sum);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
