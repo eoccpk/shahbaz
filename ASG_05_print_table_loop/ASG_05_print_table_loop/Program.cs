effeb394f608835356e25f0826c57332faa2df2c
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 04:17:27 -0700

    ASG_05_PRINT THE TABLE

---
+++ ASG_05_print_table_loop/ASG_05_print_table_loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_05_print_table_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enetr the number for the table: ");
+            int number=int.Parse(Console.ReadLine());
+            Console.WriteLine("____________________________________");
+            int mul;
+            int i = 1;
+            for(i = 1; i <= 10; i++)
+            {
+                mul = number * i;
+                Console.WriteLine(number + "*" + i +"="+ mul);
+            }
+            Console.ReadKey();
+        }
+    }
+}
