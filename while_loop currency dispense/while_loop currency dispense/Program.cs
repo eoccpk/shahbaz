9cc2c881283076a4913bdee878cc417f240d6ccd
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:38:16 -0700

    do_while loop example

---
+++ while_loop currency dispense/while_loop currency dispense/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loop_currency_dispense
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total_amount = 0;
+            while(total_amount < 1000)
+            {
+               double max= total_amount * 0.0001;
+                Console.WriteLine("total " + max);
+                total_amount++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
