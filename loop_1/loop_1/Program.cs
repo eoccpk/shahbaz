b9dab20c385684733d010c8ccacf91910391e457
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:11:14 -0700

    while loop

---
+++ loop_1/loop_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i<10)
+            {
+                Console.WriteLine("shahbaz "+i);
+                i++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
