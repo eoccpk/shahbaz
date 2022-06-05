942be59389eb73b410b978bc5c3244454229d47f
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:57:07 -0700

    ASG 01 LOOPS

---
+++ ASG_01_FOR LOOP AND WHILE LOOP/ASG_01_FOR LOOP AND WHILE LOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_01_FOR_LOOP_AND_WHILE_LOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            Console.WriteLine("enter no of times you want to ptint");
+            int num= int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("i love pakisan");
+                i++;
+            }
+            while (i < num);
+            Console.ReadKey();
+        }
+    }
+}
