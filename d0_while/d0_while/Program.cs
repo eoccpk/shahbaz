33ec23d108d9d6c0450d2235df46dec1e61bc72f
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:19:03 -0700

    do_while loop

---
+++ d0_while/d0_while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d0_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 12);
+            Console.ReadKey();
+        }
+    }
+}
