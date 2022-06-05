eca827f41aaa22e4e4fc022c559cef20176c44cc
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 05:11:03 -0700

    Functions_1

---
+++ FUNCTIONS_1/FUNCTIONS_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNCTIONS_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            print(10);
+            print(20);
+            for(int i = 0; i < 3; i++)
+            {
+                print(40);
+            }
+            Console.ReadKey();
+        }
+        static void print(int num)
+        {
+            Console.WriteLine("i am a function");
+            Console.WriteLine("call me again"+num);
+        }
+       
+    }
+}
