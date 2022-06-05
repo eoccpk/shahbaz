33177b25f1b7bd3b26ced3921b84ff04e0ad36f8
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-05 03:15:55 -0700

    for loop

---
+++ for_loop/for_loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            int sum = 0;
+            for(j = 0; j < 10; j++)
+            {
+                sum = sum + j;
+                Console.WriteLine("\t"+sum+"\n");
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
