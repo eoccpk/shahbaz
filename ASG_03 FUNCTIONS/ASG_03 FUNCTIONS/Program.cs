23159a303ccd93b412782f1a8e6d392cffaf7151
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-06 13:15:51 -0700

    ASG_03_functions area using functions

---
+++ ASG_03 FUNCTIONS/ASG_03 FUNCTIONS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_03_FUNCTIONS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the length :");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the width :");
+            double width = double.Parse(Console.ReadLine());
+            double total_area = Area(length, width);
+            Console.WriteLine("the total area is :" + total_area);
+            Console.ReadKey();
+        }
+        static double Area(double length, double width) => length * width;
+ 
+       
+    }
+}
