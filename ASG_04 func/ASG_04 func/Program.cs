2e07f1499a510c01d429513a576616d417159e9e
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-06 13:37:30 -0700

    asg_04_print table using func

---
+++ ASG_04 func/ASG_04 func/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASG_04_func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number :");
+            double number= double.Parse(Console.ReadLine());
+            Table(number);
+            Console.ReadKey();
+        }
+        static double Table(double number)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                double result = number * i;
+                Console.WriteLine(number + "*" + i + "=" + result);
+            }
+            return 0;
+        }
+    }
+}
