65733356c6936e46cf9663ae5167208ba84997d9
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-11 07:07:27 -0700

    ASG 03 classes 11/6/2022

---
+++ cricketplayer/cricketplayer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cricketplayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cricket c1 = new cricket();
+            Console.WriteLine("enter the name of the cricketer :");
+            c1.full_name = Console.ReadLine();
+            Console.WriteLine("enter the BORN DETAILS of the cricketer :");
+            c1.born = Console.ReadLine();
+            Console.WriteLine("enter the age of the cricketer :");
+            c1.age = Console.ReadLine();
+            Console.WriteLine("enter the batting style of the cricketer :");
+            c1.batting_style = Console.ReadLine();
+            Console.WriteLine("enter the bowling style of the cricketer :");
+            c1.bowling_style = Console.ReadLine();
+            Console.WriteLine("enter the relations of the cricketer :");
+            c1.relations = ("adnan akmal(cousion) kamran akmal(cousion) umer akmal(cousion)");
+            
+            c1.display();
+            c1.do_batting();
+            c1.do_bowling();
+            c1.run_sixer();
+            c1.run_bounder();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ cricketplayer/cricketplayer/cricket.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cricketplayer
+{
+    internal class cricket
+    {
+        public string full_name { get; set; }
+        public string born { get; set; }
+        public string age { get; set; }
+        public string  batting_style {get; set;}
+        public string bowling_style { get; set; }
+        public string relations { get; set; }
+        public void display()
+        {
+            Console.WriteLine("the name of the cricketer is :" + this.full_name);
+            Console.WriteLine("the born of the cricketer is :" + this.born);
+            Console.WriteLine("the batting style of the cricketer is :" + this.batting_style);
+            Console.WriteLine("the bowling style of the cricketer is :" + this.bowling_style);
+            Console.WriteLine("the relations of the cricketer is  :" + this.relations);
+            
+        }
+        
+        public void do_batting()
+        {
+            string player;
+            Console.WriteLine("enter player name to do batting :");
+            player = Console.ReadLine();
+            if(player==this.full_name)
+            {
+                Console.WriteLine(player +" do batting its tyour turn");
+            }
+            else
+                Console.WriteLine("no player found");
+        }
+        public void do_bowling()
+        {
+            string player;
+            Console.WriteLine("enter player name to do batting :");
+            player = Console.ReadLine();
+            if (player == this.full_name)
+            {
+                Console.WriteLine(player + " do bowling ");
+            }
+            else
+                Console.WriteLine("no player found");
+        }
+        public void run_sixer()
+        {
+            Console.WriteLine("its a sixer");
+        }
+        public void run_bounder()
+        {
+            Console.WriteLine("its a boundry ");
+        }
+    }
+
+}
