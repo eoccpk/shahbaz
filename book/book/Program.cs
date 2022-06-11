8a746cece0ec7d3cefddb2a4fbd25c21a682d8d0
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-11 04:37:27 -0700

    ASG_01 classes

---
+++ book/book/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            book b1=new book();
+            b1.set("physics", "ali", 100, 1990);
+            b1.print();
+            b1.borrow();
+            book b2 = new book();
+            b2.set("chamistry", "marvis", 100, 1992);
+            b2.print();
+            b2.borrow();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ book/book/book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book
+{
+    internal class book
+    {
+        string title;
+        string author;
+        int pages;
+        int pub_year;
+
+        public void set(string title, string aut, int page, int pub)
+        {
+            this.title = title;
+            this.author = aut;
+            this.pages = page;
+            this.pub_year = pub;
+        }
+        public void print()
+            {
+            Console.WriteLine("the title of book is :"+this.title);
+            Console.WriteLine("the author of book is :" + this.author);
+            Console.WriteLine("the no of pages of book is :" + this.pages);
+            Console.WriteLine("the title of pub year is :" + this.pub_year);
+        }
+        public void borrow()
+        {
+            string title;
+            Console.WriteLine("enter the title of book to borrow");
+            title=Console.ReadLine();
+
+            if (this.title==title)
+            {
+                Console.WriteLine("book is available");
+            }
+            else
+                Console.WriteLine("not available");
+        }
+
+    }
+}
