339537e359678481a27b8f26758006078edfca0f
shahbaz hussain <shahbazhussain141044@gmail.com>
2022-06-11 04:12:29 -0700

    practice classes

---
+++ classes_2/classes_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            Console.WriteLine("enter roll no : ");
+            int roll =int.Parse(Console.ReadLine());
+            Console.WriteLine("enter name :");
+            string nam = Console.ReadLine();
+            s1.set(roll, nam);
+            s1.get();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ classes_2/classes_2/student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_2
+{
+    internal class student
+    {
+        int roll_no;
+        string name;
+
+        public void set(int Roll,string Name)
+        {
+            this.name = Name;
+            this.roll_no = Roll;
+        }
+        public void get()
+        {
+            Console.WriteLine("the roll is:" + this.roll_no);
+            Console.WriteLine("the name is:" + this.name);
+        }
+    }
+}
