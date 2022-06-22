e10f8f5557b74379341b79ad81f82e04deb6efb3
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-22 23:23:32 +0500

    22/6/2022 arrays ASG 01

---
+++ arrays_ASG_01/arrays_ASG_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_ASG_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<sum_of_array> s1 = new List<sum_of_array>();
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("now new data input: ");
+                sum_of_array s2 = new sum_of_array();
+                s2.input();
+                s2.sum();
+                s1.Add(s2);
+            }
+            foreach(sum_of_array s2 in s1)
+            {
+                Console.WriteLine("shwoing the sums");
+                s2.show();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays_ASG_01/arrays_ASG_01/sum_of_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_ASG_01
+{
+    internal class sum_of_array
+    {
+        double product = 0;
+        int[] numbers = new int[5];
+        public void input()
+        {
+            Console.WriteLine("please enter the 5 numbers :");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void sum()
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                product=product+numbers[i];
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("the sum of the numbers are :"+product);
+
+        }
+
+    }
+}
