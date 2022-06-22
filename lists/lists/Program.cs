67d8977ac3329ff32fcdd475ec1736bbb1c05012
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-22 23:07:36 +0500

    practice_lists 22/5/2022

---
+++ lists/lists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<student> std = new List<student>();
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("enter the details");
+                student student = new student();
+                student.input();
+                std.Add(student);
+            }
+            Console.WriteLine("print the data of all the students ");
+            foreach(student s in std)
+            {
+                s.show();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lists/lists/student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lists
+{
+    internal class student
+    {
+        public int roll_no { get; set; }
+        public string name { get; set; }
+        public int marks { get; set; }
+        public student()
+        {
+            name = "default name";
+            roll_no = 0;
+            marks = 0;
+        }
+        public void input()
+        {
+            Console.WriteLine("enter the name of the student");
+            name=Console.ReadLine();
+            Console.WriteLine("enter the roll no");
+            roll_no=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the marks ");
+            marks=int.Parse(Console.ReadLine());
+        }
+        public void show()
+        {
+            Console.WriteLine("the name of the student is :"+name);
+            Console.WriteLine("the roll no of the student is :" + roll_no);
+            Console.WriteLine("the marks of the student is :" + marks);
+        }
+    }
+}
