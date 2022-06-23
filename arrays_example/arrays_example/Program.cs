704db1b65851c6e3d137e1746fda863f5d66f10f
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-23 21:53:26 +0500

    arrays

---
+++ arrays_example/arrays_example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arrays a1=new arrays();
+            a1.input();
+            a1.sum();
+            a1.show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays_example/arrays_example/arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_example
+{
+    internal class arrays
+    {
+        public int[] marks=new int[10];
+        public int pr;
+        public void input()
+        {
+            for(int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void sum()
+        {
+            for(int i = 0; i < marks.Length; i++)
+            {
+                pr = pr + marks[i];
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine("the result is :"+pr);
+        }
+    }
+}
