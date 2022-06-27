c1d3804624d81393f49787b4ae63e94da471f583
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-28 01:40:58 +0500

    listexample

---
+++ arraypreactice/arraypreactice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraypreactice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arraylist list = new arraylist();
+            list.Run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arraypreactice/arraypreactice/arraylist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraypreactice
+{
+    internal class arraylist
+    {
+        public void Run()
+        {
+            ArrayList marks = new ArrayList();
+            Console.WriteLine("enter the marks");
+            marks.Add(58);
+            marks.Add(33);
+            marks.Add(443);
+            Console.WriteLine("enter the ,marks more");
+            for (int i = 0; i < 4; i++)
+            {
+                marks.Add(int.Parse(Console.ReadLine()));
+            }
+            marks.Sort();
+            Console.WriteLine("print sorted");
+            for(int i = 0; i < marks.Count; i++)
+            {
+                Console.WriteLine(marks[i]);
+            }
+        }
+    }
+}
