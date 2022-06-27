11d30fdfbcb6865091817fdd34cdc94e5885cb26
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-28 01:53:10 +0500

    stringlists

---
+++ nameslist/nameslist/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameslist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            names n = new names();
+            n.run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nameslist/nameslist/names.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace nameslist
+{
+    internal class names
+    {
+        public void run()
+        {
+            ArrayList name = new ArrayList();
+            name.Add("shahbaz");
+            Console.WriteLine("enter the names");
+            for (int i = 0; i < 3; i++)
+            {
+                name.Add(Console.ReadLine());
+            }
+            if(name.Contains("shahbaz"))
+            {
+                Console.WriteLine("congrats");
+            }
+            name.Sort();
+            Console.WriteLine("print the sorted list" +
+                "");
+            foreach (string n in name)
+            {
+                Console.WriteLine("the namess are :");
+                Console.WriteLine(n);
+            }
+
+
+
+
+        }
+        
+
+
+    }
+}
