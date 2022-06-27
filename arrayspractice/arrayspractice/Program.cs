402e295b33c956ce5cc69cdb7dfb715964954d69
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-28 01:07:18 +0500

    arrays_practice

---
+++ arrayspractice/arrayspractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayspractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            examplearrays a1= new examplearrays();
+            a1.run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrayspractice/arrayspractice/examplearrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayspractice
+{
+    internal class examplearrays
+    {
+       public void run()
+        {
+            int[] marks = new int[3];
+            Console.WriteLine("enter the marks in the array");
+            for(int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("the marks at "+i+" " + marks[i]);
+            }
+        }
+       
+
+    }
+}
