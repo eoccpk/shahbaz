8027de69c5bb4d0291df23d5c51dcdddc4616cc2
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-28 01:14:40 +0500

    updated arrays

--- arrayspractice/arrayspractice/examplearrays.cs
+++ arrayspractice/arrayspractice/examplearrays.cs
@@ -10,16 +10,27 @@ namespace arrayspractice
     {
        public void run()
         {
+            
             int[] marks = new int[3];
+            
             Console.WriteLine("enter the marks in the array");
             for(int i = 0; i < marks.Length; i++)
             {
                 marks[i] = int.Parse(Console.ReadLine());
             }
-            for(int i = 0; i < marks.Length; i++)
+            int max = marks[0];
+            for (int i = 0; i < marks.Length; i++)
             {
+                
                 Console.WriteLine("the marks at "+i+" " + marks[i]);
+                
+                if(marks[i] > max)
+                {
+                    max = marks[i];
+                }
+                
             }
+            Console.WriteLine("the greatest number is "+ max);
         }
        
 
