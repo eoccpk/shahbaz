933c2afebed34fb3949781fa8aed2dd1b7576892
shahbaz141 <105984933+shahbaz141@users.noreply.github.com>
2022-06-29 19:11:58 +0500

    example using lists

---
+++ BANK ACCOUNT ASG USING LIST/BANK ACCOUNT ASG USING LIST/BANK_ACC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK_ACCOUNT_ASG_USING_LIST
+{
+    internal class BANK_ACC
+    {
+        public string accountitle { get; set; }
+        public string accountno { get; set; }
+
+        public BANK_ACC()
+        {
+            accountitle = "default";
+            accountno = "0000";
+        }
+        public void input()
+        {
+            Console.WriteLine("enter account title");
+            accountitle = Console.ReadLine();
+            Console.WriteLine("enter the account no");
+            accountno = Console.ReadLine();
+        }
+        public void display()
+        {
+            Console.WriteLine("the account title is "+accountitle);
+            Console.WriteLine("the account no is "+accountno);
+        }
+    }
+}
---
+++ BANK ACCOUNT ASG USING LIST/BANK ACCOUNT ASG USING LIST/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK_ACCOUNT_ASG_USING_LIST
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<BANK_ACC> A1 = new List<BANK_ACC>();
+            for (int i = 0; i < 2; i++)
+            {
+                BANK_ACC stu = new BANK_ACC();
+                stu.input();
+                A1.Add(stu);
+            }
+            foreach(BANK_ACC ad in A1)
+            {
+                ad.display();  
+            }
+            Console.ReadKey();
+        }
+    }
+}
